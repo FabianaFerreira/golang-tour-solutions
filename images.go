bb7656ede132171b88829a56337cc8289e44fcc4
FabianaFerreira <fabiana.ferreira@poli.ufrj.br>
2021-04-28 14:28:25 -0300

    Changing implementation to use pointers

--- images.go
+++ images.go
@@ -11,15 +11,15 @@ type Image struct {
 	width, height int
 }
 
-func (im Image) ColorModel() color.Model {
+func (im *Image) ColorModel() color.Model {
 	return color.RGBAModel
 }
 
-func (im Image) Bounds() image.Rectangle {
+func (im *Image) Bounds() image.Rectangle {
 	return image.Rect(0, 0, im.width, im.height)
 }
 
-func (im Image) At(x, y int) color.Color {
+func (im *Image) At(x, y int) color.Color {
 	v := uint8((x + y) / 2)
 
 	return color.RGBA{v, v, 255, 255}
@@ -27,5 +27,5 @@ func (im Image) At(x, y int) color.Color {
 
 func images() {
 	m := Image{100, 100}
-	pic.ShowImage(m)
+	pic.ShowImage(&m)
 }
